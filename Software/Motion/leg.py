0b1aa1a6d3f5cf9ff92294604f122dc2a69f3990
Keith Elliott <k.w.elliott121@gmail.com>
2020-05-17 21:35:57 -0400

    Adding subdir for leg config files.

--- Software/Motion/leg.py
+++ Software/Motion/leg.py
@@ -1,17 +1,21 @@
 from joint import Joint, DummyServo
 from segment import Segment
+from os import makedirs, path
 
 # Class that represents a 3-jointed leg for the SpotMicro robot dog
 class Leg:
     def __init__(self, leg_name, offset, servos):
         self.leg_name = leg_name
+        if not path.exists(leg_name):
+            makedirs(leg_name)
+        
         self.offset = offset
-        self.upper_hip = Joint(servos["upper_hip"], leg_name + "upper_hip.json")
-        self.hip = Segment(leg_name + "hip.json")
-        self.lower_hip = Joint(servos["lower_hip"], leg_name + "lower_hip.json")
-        self.femur = Segment(leg_name + "femur.json")
-        self.knee = Joint(servos["knee"], leg_name + "knee.json")
-        self.tibia = Segment(leg_name + "tibia.json")
+        self.upper_hip = Joint(servos["upper_hip"], leg_name + "/upper_hip.json")
+        self.hip = Segment(leg_name + "/hip.json")
+        self.lower_hip = Joint(servos["lower_hip"], leg_name + "/lower_hip.json")
+        self.femur = Segment(leg_name + "/femur.json")
+        self.knee = Joint(servos["knee"], leg_name + "/knee.json")
+        self.tibia = Segment(leg_name + "/tibia.json")
         
     def store(self):
         self.upper_hip.store()
